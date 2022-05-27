03e6a5d713e76107cac9f71b1b7f8383ba057a3d
petmik1 <petter.mikalsen.pehm@gmail.com>
2022-05-27 18:25:35 +0200

    update

--- js/components/html.js
+++ js/components/html.js
@@ -12,15 +12,9 @@ function createHtmlIndex(result, i, selected, resultMedia) {
     dotSection.innerHTML += `<div class="dot ${selected}Dot"></div>`;
 }
 // html for the blog specific page
-function createHtmlBlogSpecific  (awewedwddddddddw) {
-    console.log(resultMedia)
-    carrusel.innerHTML += `
-    <a href="pages/blog-spesific.html?id=${result[i].id}" class="carruselContainer ${selected}">
-        <h1>${result[i].title.rendered}</h1>
-        <img src="${resultMedia[0].source_url}" class="carruselImage" alt="">
-        <p>${result[i].excerpt.rendered}</p>
-        
-    </a>
-    `
-    dotSection.innerHTML += `<div class="dot ${selected}Dot"></div>`;
+function createHtmlBlogSpecific  (result, i) {
+    console.log(result[i].title)
+    content.innerHTML = `<h2>${result[i].title.rendered}</h2>
+    <div class="figure">${result[i].content.rendered}</div>`
+    
 }
