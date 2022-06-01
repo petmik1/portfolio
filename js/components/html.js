4f15729708f5aece34c34eacb651ddf4d24d7154
petmik1 <petter.mikalsen.pehm@gmail.com>
2022-06-01 14:06:18 +0200

    worked on making it responsive

--- js/components/html.js
+++ js/components/html.js
@@ -3,9 +3,7 @@ function createHtmlIndex(result, i, selected, resultMedia) {
     carrusel.innerHTML += `
     <a href="pages/blog-spesific.html?id=${result[i].id}" class="carruselContainer ${selected}">
         <h1>${result[i].title.rendered}</h1>
-        <img src="${resultMedia[0].source_url}" class="carruselImage" alt="">
-        <p>${result[i].excerpt.rendered}</p>
-        
+        <img src="${resultMedia[0].source_url}" class="carruselImage" alt="">        
     </a>
     `
     dotSection.innerHTML += `<div class="dot ${selected}Dot"></div>`;
